24c41441de8ff199d9f23c7d1d4aeccfb8c207e2
soutaku Shin <37830914+ShinATK@users.noreply.github.com>
2023-08-24 19:00:49 +0800

    Update 2023-8-24
    重新整理了目前学习到的Python机器学习中的部分代码

---
+++ Python_Machine_Learning_Code/utils/samples.py
@@ -0,0 +1,19 @@
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+iris = datasets.load_iris()
+
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
